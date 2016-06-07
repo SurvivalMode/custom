a380ca774ae637900c1a4d6c97b991fd143fea38
SurvivalMode <im@matty.pw>
2016-06-07 18:33:16 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -85,7 +85,7 @@
       ["sound", "The song you played had bad sound quality or no sound. "],
       ["nsfw", "The song you contained was NSFW (image or sound). "],
       ["unavailable", "The song you played was not available for some users. "],
-      ["other", "Your song was skipped!. "]
+      ["other", "Your song was skipped! "]
       ],
       afkpositionCheck: 15,
       afkRankCheck: "ambassador",
